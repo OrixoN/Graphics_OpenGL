3463b63f4d556ad0436e5063d4002c5eb4710514
OrixoN <300885voltik@gmail.com>
2026-02-24 22:54:22 +0200

    Task01: Initial state, project created and OpenTK installed

---
+++ Tasks/Task01/Game.cs
@@ -0,0 +1,30 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Task01
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title)
+            : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            if (KeyboardState.IsKeyDown(Keys.Escape)) Close();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            
+
+            SwapBuffers();
+        }
+    }
+}
---
+++ Tasks/Task01/Program.cs
@@ -0,0 +1,6 @@
+﻿using Task01;
+
+using (Game game = new Game(800, 600, "Lab 01 - Kuzmenko D.O. - Variant 07"))
+{
+    game.Run();
+}
