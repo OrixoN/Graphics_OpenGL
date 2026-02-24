5b889ada11959dbbd62e0841f65bacce0e1daa4e
OrixoN <300885voltik@gmail.com>
2026-02-24 23:24:47 +0200

    Finalized Task01: Correct hexagon shape for Variant 7

--- Tasks/Task01/Game.cs
+++ Tasks/Task01/Game.cs
@@ -1,28 +1,75 @@
-﻿using OpenTK.Graphics.OpenGL4;
+﻿using OpenTK.Graphics.OpenGL;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Mathematics;
+using System;
 
 namespace Task01
 {
     public class Game : GameWindow
     {
-        public Game(int width, int height, string title)
-            : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
+        public Game(NativeWindowSettings settings)
+            : base(GameWindowSettings.Default, settings) { }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             base.OnUpdateFrame(e);
             if (KeyboardState.IsKeyDown(Keys.Escape)) Close();
         }
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
-            GL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            GL.ClearColor(Color4.White);
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
-            
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 4.0, -1.0, 6.0, -1.0, 1.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            GL.Color3(0.8f, 0.8f, 0.8f);
+            GL.Begin(PrimitiveType.Lines);
+            for (int i = -1; i <= 4; i++) { GL.Vertex2(i, -1); GL.Vertex2(i, 6); }
+            for (int i = -1; i <= 6; i++) { GL.Vertex2(-1, i); GL.Vertex2(4, i); }
+            GL.End();
+
+            Vector2[] points = new Vector2[]
+            {
+        new Vector2(1, 1),
+        new Vector2(2, 2),
+        new Vector2(3, 4),
+        new Vector2(2, 5),
+        new Vector2(1, 5),
+        new Vector2(0, 4) 
+            };
+
+            GL.LineWidth(4.0f);
+            GL.Color3((System.Drawing.Color)Color4.Blue);
+            GL.Begin(PrimitiveType.LineLoop);
+            foreach (var p in points)
+            {
+                GL.Vertex2(p);
+            }
+            GL.End();
+
+            GL.Enable(EnableCap.PointSmooth);
+            GL.PointSize(15.0f);
+            GL.Color3((System.Drawing.Color)Color4.Red);
+            GL.Begin(PrimitiveType.Points);
+            foreach (var p in points)
+            {
+                GL.Vertex2(p);
+            }
+            GL.End();
+            GL.Disable(EnableCap.PointSmooth);
 
             SwapBuffers();
         }
--- Tasks/Task01/Program.cs
+++ Tasks/Task01/Program.cs
@@ -1,6 +1,16 @@
-﻿using Task01;
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using Task01;
 
-using (Game game = new Game(800, 600, "Lab 01 - Kuzmenko D.O. - Variant 07"))
+var nativeWindowSettings = new NativeWindowSettings()
+{
+    ClientSize = (800, 600),
+    Title = "Lab 01 - Kuzmenko D.O. - Variant 07",
+    Profile = ContextProfile.Compatability,
+    Flags = ContextFlags.Default
+};
+
+using (Game game = new Game(nativeWindowSettings))
 {
     game.Run();
 }
